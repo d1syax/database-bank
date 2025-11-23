c8cfa90d80d4251fc00c104b115cbb014b1ba354
d1syax <boyaka201@gmail.com>
2025-11-23 21:08:41 +0200

    feat: create dto res for transaction

---
+++ MyBank.Api/DTOs/Responses/TransactionResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace MyBank.Api.DTOs.Responses;
+
+public record TransactionResponse(
+    Guid Id,
+    Guid FromAccountId,
+    Guid ToAccountId,
+    decimal Amount,
+    string TransactionType,
+    string Status,
+    string Description,
+    DateTime CreatedAt
+);
