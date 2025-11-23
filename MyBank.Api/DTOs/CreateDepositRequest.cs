08801e10a1bb1a0991515778e349f82cb5d5547f
d1syax <boyaka201@gmail.com>
2025-11-23 21:01:53 +0200

    feat: create dto for deposit

---
+++ MyBank.Api/DTOs/CreateDepositRequest.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBank.Api.DTOs;
+
+public record DepositRequest(
+    [property: Required] 
+    [property: Range(0.01, 10000000)] 
+    decimal Amount
+);
