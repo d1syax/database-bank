a826b1dba4f0310949cd956105ef671433752397
d1syax <boyaka201@gmail.com>
2025-11-23 11:48:53 +0200

    feat: create loan payment configurations

---
+++ MyBank.Infrastructure/Persistance/Configurations/LoanPaymentConfigurator.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBank.Domain.Entities;
+
+namespace MyBank.Infrastructure.Persistance.Configurations;
+
+public class LoanPaymentConfigurator : IEntityTypeConfiguration<LoanPaymentEntity>
+{
+    public void Configure(EntityTypeBuilder<LoanPaymentEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Amount).HasPrecision(18, 2);
+
+        builder.HasOne(x => x.LoanEntity)
+            .WithMany(x => x.Payments)
+            .HasForeignKey(x => x.LoanId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
