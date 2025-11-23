79b3f06145c50dc431937a6c75de234fdd37f592
d1syax <boyaka201@gmail.com>
2025-11-23 20:58:18 +0200

    feat: create dto for account

---
+++ MyBank.Api/DTOs/CreateAccoutRequest.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DefaultNamespace;
+
+namespace MyBank.Api.DTOs;
+
+public record CreateAccountRequest(
+    [property: Required] 
+    Guid UserId,
+    
+    [property: Required] 
+    [property: StringLength(3, MinimumLength = 3)] 
+    string Currency,
+    
+    [property: Required] 
+    AccountType AccountType
+);
