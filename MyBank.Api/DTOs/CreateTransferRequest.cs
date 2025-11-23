ab06ab897b270199f5c12a8e0db693ce5f782de1
d1syax <boyaka201@gmail.com>
2025-11-23 20:59:57 +0200

    feat: create dto for transfer

---
+++ MyBank.Api/DTOs/CreateTransferRequest.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBank.Api.DTOs;
+
+public record CreateTransferRequest(
+    [property: Required] 
+    Guid FromAccountId,
+    
+    [property: Required] 
+    Guid ToAccountId,
+    
+    [property: Required] 
+    [property: Range(0.01, 1000000)] 
+    decimal Amount,
+    
+    [property: StringLength(100)] 
+    string Description = "Transfer" 
+);
