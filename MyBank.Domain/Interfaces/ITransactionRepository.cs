b6d5a658ef66d0c4d1d76bae1d11e061eda44fe1
d1syax <boyaka201@gmail.com>
2025-11-23 16:11:07 +0200

    feat: create transaction repository

--- MyBank.Domain/Interfaces/ITransactionRepository.cs
+++ MyBank.Domain/Interfaces/ITransactionRepository.cs
@@ -4,8 +4,10 @@ namespace MyBank.Domain.Interfaces;
 
 public interface ITransactionRepository
 {
-    Task<TransactionEntity> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<List<TransactionEntity>> GetByAccountIdAsync(Guid accountId, int limit = 20, CancellationToken cancellationToken = default);
-
-    void Add(TransactionEntity transaction);
+    Task<TransactionEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+    Task<List<TransactionEntity>> GetByAccountIdAsync(Guid accountId, CancellationToken cancellationToken = default);
+    Task<List<TransactionEntity>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
+    Task<List<TransactionEntity>> GetByDateRangeAsync(DateTime startDate, DateTime endDate, CancellationToken cancellationToken = default);
+    Task AddAsync(TransactionEntity transaction, CancellationToken cancellationToken = default);
+    Task UpdateAsync(TransactionEntity transaction, CancellationToken cancellationToken = default);
 }
