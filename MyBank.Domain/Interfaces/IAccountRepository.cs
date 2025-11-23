f7b8e9fcba1a685d949335d0875bdb10e8f3a03a
d1syax <boyaka201@gmail.com>
2025-11-23 15:36:21 +0200

    feat: create account repository

--- MyBank.Domain/Interfaces/IAccountRepository.cs
+++ MyBank.Domain/Interfaces/IAccountRepository.cs
@@ -5,10 +5,9 @@ namespace MyBank.Domain.Interfaces;
 public interface IAccountRepository
 {
     Task<AccountEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
-    Task<AccountEntity> GetByIdWithUserAsync(Guid id, CancellationToken cancellationToken = default);
     Task<List<AccountEntity>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default);
     Task<AccountEntity> GetByAccountNumberAsync(string accountNumber, CancellationToken cancellationToken = default);
-
-    void Add(AccountEntity account);
-    void Update(AccountEntity account);
+    Task AddAsync(AccountEntity account, CancellationToken cancellationToken = default);
+    Task UpdateAsync(AccountEntity account, CancellationToken cancellationToken = default);
+    Task DeleteAsync(AccountEntity account, CancellationToken cancellationToken = default);
 }
---
+++ MyBank.Infrastructure/Persistence/Repositories/AccountRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBank.Domain.Entities;
+using MyBank.Domain.Interfaces;
+using MyBank.Infrastructure.Persistance;
+
+namespace MyBank.Infrastructure.Persistence.Repositories;
+
+public class AccountRepository : IAccountRepository
+{
+    public AccountRepository(BankDbContext context)
+    {
+        _context = context;
+    }
+
+    private readonly BankDbContext _context;
+    
+    public async Task<AccountEntity?> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+    {
+        return await _context.Accounts.FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+    }
+
+    public async Task<List<AccountEntity>> GetByUserIdAsync(Guid userId, CancellationToken cancellationToken = default)
+    {
+        return await _context.Accounts.Where(x => x.UserId == userId).ToListAsync(cancellationToken);
+    }
+
+    public async Task<AccountEntity?> GetByAccountNumberAsync(string accountNumber, CancellationToken cancellationToken = default)
+    {
+        return await _context.Accounts.FirstOrDefaultAsync(x => x.AccountNumber == accountNumber, cancellationToken);
+    }
+
+    public async Task AddAsync(AccountEntity account, CancellationToken cancellationToken = default)
+    {
+        await _context.Accounts.AddAsync(account, cancellationToken);
+    }
+
+    public Task UpdateAsync(AccountEntity account, CancellationToken cancellationToken = default)
+    {
+        _context.Accounts.Update(account);
+        return Task.CompletedTask;
+    }
+
+    public Task DeleteAsync(AccountEntity account, CancellationToken cancellationToken = default)
+    {
+        _context.Accounts.Update(account);
+        return Task.CompletedTask;
+    }
+} 
