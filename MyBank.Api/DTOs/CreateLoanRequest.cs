37ce60ff5ea49c67d31a7ceda57aee8f87b2789c
d1syax <boyaka201@gmail.com>
2025-11-23 21:06:11 +0200

    feat: create dto req for loan

---
+++ MyBank.Api/DTOs/CreateLoanRequest.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBank.Api.DTOs;
+
+public record CreateLoanRequest(
+    [property: Required] 
+    Guid UserId,
+
+    [property: Required] 
+    Guid AccountId, 
+
+    [property: Required] 
+    [property: Range(100, 10000000)] 
+    decimal Amount,
+
+    [property: Required] 
+    [property: Range(0.1, 100)] 
+    decimal InterestRate 
+);
