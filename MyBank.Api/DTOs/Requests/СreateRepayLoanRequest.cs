9eaa26fa09500d5a0614419894625183288150ea
d1syax <boyaka201@gmail.com>
2025-11-23 21:14:17 +0200

    feat: create dto req for repay loan

---
+++ MyBank.Api/DTOs/Requests/СreateRepayLoanRequest.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBank.Api.DTOs;
+
+public record RepayLoanRequest(
+    [property: Required]
+    Guid LoanId,
+
+    [property: Required]
+    [property: Range(0.01, 10000000)]
+    decimal Amount
+);
