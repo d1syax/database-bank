49ad90019467c9eadfae02f55267ea1e2184d7ac
d1syax <boyaka201@gmail.com>
2025-11-23 11:39:29 +0200

    feat: create loan configurator

---
+++ MyBank.Infrastructure/Persistance/Configurations/LoanConfigurator.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using MyBank.Domain.Entities;
+
+namespace MyBank.Infrastructure.Persistance.Configurations;
+
+public class LoanConfigurator : IEntityTypeConfiguration<LoanEntity>
+{
+    public void Configure(EntityTypeBuilder<LoanEntity> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.PrincipalAmount).HasPrecision(18, 2);
+        builder.Property(x => x.InterestAmount).HasPrecision(18, 2);
+        builder.Property(x => x.PaidAmount).HasPrecision(18, 2);
+        
+        builder.Property(x => x.Status).HasConversion<string>();
+
+        builder.HasOne(x => x.UserEntity)
+            .WithMany(x => x.Loans)
+            .HasForeignKey(x => x.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(x => x.AccountEntity)
+            .WithMany(x => x.Loans)
+            .HasForeignKey(x => x.AccountId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
