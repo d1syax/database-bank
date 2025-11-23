e41d6b54b24e73ac6506af358de9ceaa4262b67d
d1syax <boyaka201@gmail.com>
2025-11-23 21:05:25 +0200

    feat: create dto req for card

---
+++ MyBank.Api/DTOs/CreateCardRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+using DefaultNamespace;
+
+public record CreateCardRequest(
+    [property: Required] 
+    Guid AccountId,
+
+    [property: Required] 
+    CardType CardType
+);
