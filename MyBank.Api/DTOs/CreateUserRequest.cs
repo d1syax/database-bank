37f016cc429d86cf079dc835eda71e63de8c0985
d1syax <boyaka201@gmail.com>
2025-11-23 20:56:55 +0200

    feat: create dto for user

---
+++ MyBank.Api/DTOs/CreateUserRequest.cs
@@ -0,0 +1,27 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyBank.Api.DTOs;
+
+public record CreateUserRequest(
+    [property: Required] 
+    [property: StringLength(50)] 
+    string FirstName,
+    
+    [property: Required] 
+    [property: StringLength(50)] 
+    string LastName,
+    
+    [property: Required] 
+    [property: EmailAddress] 
+    string Email,
+    
+    [property: Required] 
+    [property: Phone]
+    string Phone,
+    
+    [property: Required] 
+    string Password,
+    
+    [property: Required] 
+    DateTime DateOfBirth
+);
