e1ae20bc8c59e2ed64b72c18c11b2b5bce7d7105
d1syax <boyaka201@gmail.com>
2025-12-15 18:16:23 +0200

    feat: create new sln for tests, add testbast & user tests

---
+++ MyBank.Tests/TestBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyBank.Infrastructure.Persistence;
+using MyBank.Infrastructure.Persistence.Repositories;
+using MyBank.Domain.Interfaces;
+
+namespace MyBank.Tests;
+
+public class TestBase : IDisposable
+{
+    protected readonly BankDbContext Context;
+    protected readonly IUserRepository UserRepository;
+    protected readonly IAccountRepository AccountRepository;
+    protected readonly ICardRepository CardRepository;
+    protected readonly ILoanRepository LoanRepository;
+    protected readonly ITransactionRepository TransactionRepository;
+    protected readonly IUnitOfWork UnitOfWork;
+
+    public TestBase()
+    {
+        var options = new DbContextOptionsBuilder<BankDbContext>()
+            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+            .Options;
+
+        Context = new BankDbContext(options);
+        
+        UserRepository = new UserRepository(Context);
+        AccountRepository = new AccountRepository(Context);
+        CardRepository = new CardRepository(Context);
+        LoanRepository = new LoanRepository(Context);
+        TransactionRepository = new TransactionRepository(Context);
+        UnitOfWork = new UnitOfWork(Context);
+    }
+
+    public void Dispose()
+    {
+        Context.Database.EnsureDeleted();
+        Context.Dispose();
+    }
+}
---
+++ MyBank.Tests/testsDomain/UserEntityTests.cs
@@ -0,0 +1,70 @@
+﻿using FluentAssertions;
+using MyBank.Domain.Entities;
+using Xunit;
+
+namespace MyBank.Tests.Domain;
+
+public class UserEntityTests
+{
+    [Fact]
+    public void Create_WithValidData_ShouldSucceed()
+    {
+        var firstName = "Pavlo";
+        var lastName = "Leheza";
+        var email = "pavloleheza@gmail.com";
+        var passwordHash = "hashed123";
+        var dateOfBirth = new DateTime(1995, 2, 1);
+        var phone = "+380991234563";
+
+        var result = UserEntity.Create(firstName, lastName, email, passwordHash, dateOfBirth, phone);
+
+        result.IsSuccess.Should().BeTrue();
+        result.Value.FirstName.Should().Be(firstName);
+        result.Value.LastName.Should().Be(lastName);
+        result.Value.Email.Should().Be(email);
+        result.Value.Age.Should().BeGreaterThan(14);
+    }
+
+    [Fact]
+    public void Create_WithEmptyEmail_ShouldFail()
+    {
+        var result = UserEntity.Create("Pavlo", "Leheza", "", "hash123", new DateTime(1995, 2, 1), "+380991234563");
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Contain("Email");
+    }
+
+    [Fact]
+    public void Create_WithTooYoungAge_ShouldFail()
+    {
+        var dateOfBirth = DateTime.UtcNow.AddYears(-10); 
+        
+        var result = UserEntity.Create("Pavlo", "Leheza", "pavloleheza@gmail.com", "hash123", dateOfBirth, "+380991234563");
+
+        result.IsFailure.Should().BeTrue();
+        result.Error.Should().Contain("young");
+    }
+
+    [Fact]
+    public void UpdateProfile_WithValidData_ShouldSucceed()
+    {
+        var user = UserEntity.Create("Pavlo", "Leheza", "pavloleheza@gmail.com", "hash123", new DateTime(1995, 2, 1), "+380991234563").Value;
+        
+        var result = user.UpdateProfile("Pavlo", "Leheza", "+380991111111");
+        
+        result.IsSuccess.Should().BeTrue();
+        user.FirstName.Should().Be("Pavlo");
+        user.LastName.Should().Be("Leheza");
+        user.PhoneNumber.Should().Be("+380991111111");
+    }
+
+    [Fact]
+    public void FullName_ShouldCombineFirstAndLastName()
+    {
+        var user = UserEntity.Create("Pavlo", "Leheza", "pavloleheza@gmail.com", "hash123", new DateTime(1995, 2, 1), "+380991234563").Value;
+
+        var fullName = user.FullName;
+
+        fullName.Should().Be("Pavlo Leheza");
+    }
+}
