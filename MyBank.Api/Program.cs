0d8b19cc640db70f08e6ed7a7cc543c8f652db98
d1syax <boyaka201@gmail.com>
2025-12-16 09:57:57 +0200

    feat: create laucnhsettings

--- MyBank.Api/Program.cs
+++ MyBank.Api/Program.cs
@@ -29,9 +29,12 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
-
-app.UseSwagger(); 
-app.UseSwaggerUI();
+ 
+if (app.Environment.IsDevelopment())
+{
+	app.UseSwagger(); 
+	app.UseSwaggerUI();
+}
 
 app.UseMiddleware<ExceptionHandlingMiddleware>();
 
